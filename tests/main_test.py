907c0af4242c744a75522b6d8261f0fd8d6ccad4
Matt Olson <SirIndubitable@gmail.com>
2021-04-21 18:32:14 -0500

    Add some simple code getting google calander information and a test for it

---
+++ tests/main_test.py
@@ -0,0 +1,8 @@
+import pytest
+import sys, os
+sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
+from mcu_calendar.main import *
+
+def test_google_login():
+    creds = get_google_creds()
+    assert creds
