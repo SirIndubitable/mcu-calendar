4b9b46aeaf63c201eaacafe2ef27e3aff1815f36
Matt Olson <SirIndubitable@gmail.com>
2021-04-25 13:33:25 -0500

    Make movies into a class

--- mcu_calendar/main.py
+++ mcu_calendar/main.py
@@ -44,33 +44,55 @@ def get_google_events():
     return events_result.get('items', [])
 
 
+class Movie():
+    def __init__(self, yaml_path):
+        with open(yaml_path, 'r') as yaml_file:
+            yaml_dict = yaml.load(yaml_file, Loader=yaml.Loader)
+            self.__dict__.update(yaml_dict)
+
+    def to_google_event(self):
+        return {
+            "start": { "date": self.release_date.isoformat() },
+            "end": { "date": self.release_date.isoformat() },
+            "summary": self.title,
+            "description": self.description,
+        }
+
+    def __eq__(self, other):
+        if isinstance(other, Movie):
+            return self.title        == other.title       \
+               and self.description  == other.description \
+               and self.release_date == other.release_date
+        else:
+            self_event = self.to_google_event()
+            return self_event["start"]["date"] == other["start"]["date"] \
+               and self_event["end"]["date"]   == other["end"]["date"]   \
+               and self_event["summary"]       == other["summary"]       \
+               and self_event["description"]   == other["description"]
+
+
+    def __ne__(self, other):
+        return not self == other
+
+
+    def __str__(self):
+        return f"{truncate(self.title, 26)} {self.release_date.strftime('%b %d, %Y')}"
+
+
 def get_movies():
-    movies = []
-    for filename in os.listdir(os.path.join('data', 'movies')):
-        with open(os.path.join('data', 'movies', filename), 'r') as movie_yaml:
-            yaml_movie = yaml.load(movie_yaml, Loader=yaml.Loader)
-            movies.append({
-                "start": { "date": yaml_movie['release_date'].isoformat() },
-                "end": { "date": yaml_movie['release_date'].isoformat() },
-                "summary": yaml_movie['title'],
-                "description": yaml_movie['description']
-            })
-    return movies
+    movies_dir = os.path.join('data', 'movies')
+    return [Movie(os.path.join(movies_dir, filename)) for filename in os.listdir(movies_dir)]
 
 
 def truncate(string, length):
     return string[:(length-3)].ljust(length, ".")
 
 
-def find(f, seq):
+def find(seq, f):
     for item in seq:
         if f(item):
             return item
     return None
-
-
-def format_item(item):
-    return f"[reset]{truncate(item['summary'], 26)} {datetime.date.fromisoformat(item['start']['date']).strftime('%b %d, %Y')}"
 
 
 def create_google_event(progressTitle, items, existingEvents):
@@ -79,22 +101,20 @@ def create_google_event(progressTitle, items, existingEvents):
         BarColumn(),
         "[progress.percentage]{task.percentage:>3.0f}%",
         TimeElapsedColumn())
-    items.sort(key=lambda m: m['start']['date'])
+    items.sort(key=lambda m: m.release_date)
     with progress:
         for item in progress.track(items):
-            event = find(lambda e: e['summary'] == item['summary'], existingEvents)
+            event = find(existingEvents, lambda e: e['summary'] == item.title)
             if event == None:
-                progress.print(format_item(item), "[red](Adding)")
-                events_service.insert(calendarId=calId, body=item).execute()
-            elif event["start"]["date"] != item["start"]["date"] \
-              or event['description'] != item['description']:
-                progress.print(format_item(item), "[yellow](Updating)")
-                events_service.update(calendarId=calId, eventId=event["id"], body=item).execute()
+                progress.print(f"[reset]{item}", "[red](Adding)")
+                events_service.insert(calendarId=calId, body=item.to_google_event()).execute()
+            elif item != event:
+                progress.print(f"[reset]{item}", "[yellow](Updating)")
+                events_service.update(calendarId=calId, eventId=event["id"], body=item.to_google_event()).execute()
             else:
-                progress.print(format_item(item), "[cyan](Skipping)")
+                progress.print(f"[reset]{item}", "[cyan](Skipping)")
 
 
-import time
 calId = get_cal_id()
 creds = get_google_creds()
 events_service = build('calendar', 'v3', credentials=creds).events()
@@ -103,7 +123,6 @@ def main():
     global events_service
     events = get_google_events()
     create_google_event("Movies...", get_movies(), events)
-
 
 if __name__ == '__main__':
     main()
