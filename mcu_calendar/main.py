5c834227df095f7e4abddfb50e46b140af2d77d7
Matt Olson <SirIndubitable@gmail.com>
2021-04-28 23:17:47 -0500

    Address lint issues

--- mcu_calendar/main.py
+++ mcu_calendar/main.py
@@ -1,3 +1,6 @@
+"""
+This script adds events to a google users calendar for Movies and TV shows defined in ./data/
+"""
 import datetime
 import os
 from googleapiclient.discovery import build
@@ -12,25 +15,32 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
 
 def update_creds_token(creds):
+    """
+    Updates the token.json containing the login token so that when testing
+    it is not required to login constantly
+    """
     with open('token.json', 'w') as token:
-       token.write(creds.to_json())
+        token.write(creds.to_json())
 
 
 def get_google_creds():
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    """
+    Gets the google Credentials object in this order:
+    1. Automatically created token.json
+    2. credentials.json file created from https://console.cloud.google.com/
+    3. Environment variables containing the token information
+    """
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
         if creds.valid:
             return creds
-        elif creds.expired and creds.refresh_token:
+        if creds.expired and creds.refresh_token:
             creds.refresh(Request())
             update_creds_token(creds)
             return creds
 
-    # This is the credentials file that is created from making a project with an 
-    # OAuth 2.0 Client ID from https://console.cloud.google.com/
+    # This is the credentials file that is created from making a project with an
+    # OAuth 2.0 Client ID from
     if os.path.exists('credentials.json'):
         flow = InstalledAppFlow.from_client_secrets_file(
             client_secrets_file='credentials.json',
@@ -56,8 +66,13 @@ def get_google_creds():
 
 
 def get_cal_id():
-    # The actual mcu calendar uses it's "primary" calendar
-    # Though we have two options to override this for testing purposes
+    """
+    Gets the ID of the calender that this script should write to.  This ID should
+    belong to the user that logged in from get_google_creds()
+    1. From the cal_id.txt file
+    2. From GOOGLE_MCU_CALENDAR_ID environment variable
+    3. The primary user calendar
+    """
     if os.path.exists('cal_id.txt'):
         with open('cal_id.txt', 'r') as reader:
             return reader.read().strip()
@@ -67,34 +82,49 @@ def get_cal_id():
 
 
 def get_google_events():
-    global service
-    global calId
-    now = datetime.datetime.utcnow().isoformat() + 'Z'
-    events_result = events_service.list(calendarId=calId).execute()
+    """
+    Gets all of the events currently on the calender from get_cal_id()
+    """
+    events_result = EVENTS_SERVICE.list(calendarId=get_cal_id()).execute()
     return events_result.get('items', [])
 
 
-class GoogleMediaEvent(object):
-    def __init__(self, args):
-        if isinstance(args, str):
-            with open(args, 'r') as yaml_file:
-                yaml_dict = yaml.load(yaml_file, Loader=yaml.Loader)
-                self.__init__(yaml_dict)
-        if isinstance(args, dict):
-            self.__dict__.update(args)
-
+class GoogleMediaEvent():
+    """
+    Base class for a google event that is defined in yaml
+    """
     def to_google_event(self):
-        pass
+        """
+        Converts this object to a google calendar api event
+        https://developers.google.com/calendar/v3/reference/events#resource
+        """
 
     def sort_val(self):
-        pass
+        """
+        Gets the value that this object should be sorted by
+        """
 
     def __ne__(self, other):
         return not self == other
 
 
 class Movie(GoogleMediaEvent):
-    sort_key = "release_date"
+    """
+    The event that describes a movie release date
+    """
+    def __init__(self, title, description, release_date):
+        self.title = title
+        self.description = description
+        self.release_date = release_date
+
+    @staticmethod
+    def from_yaml(yaml_path):
+        """
+        Factory method to create a Movie object from yaml
+        """
+        with open(yaml_path, 'r') as yaml_file:
+            yaml_data = yaml.load(yaml_file, Loader=yaml.Loader)
+        return Movie(**yaml_data)
 
     def to_google_event(self):
         return {
@@ -112,18 +142,34 @@ class Movie(GoogleMediaEvent):
             return self.title        == other.title       \
                and self.description  == other.description \
                and self.release_date == other.release_date
-        else:
-            self_event = self.to_google_event()
-            return self_event["start"]["date"] == other["start"]["date"] \
-               and self_event["end"]["date"]   == other["end"]["date"]   \
-               and self_event["summary"]       == other["summary"]       \
-               and self_event["description"]   == other["description"]
+        self_event = self.to_google_event()
+        return self_event["start"]["date"] == other["start"]["date"] \
+           and self_event["end"]["date"]   == other["end"]["date"]   \
+           and self_event["summary"]       == other["summary"]       \
+           and self_event["description"]   == other["description"]
 
     def __str__(self):
         return f"{truncate(self.title, 26)} {self.release_date.strftime('%b %d, %Y')}"
 
 
 class Show(GoogleMediaEvent):
+    """
+    The event that describes a show start date and how many weeks it runs for
+    """
+    def __init__(self, title, start_date, weeks):
+        self.title = title
+        self.start_date = start_date
+        self.weeks = weeks
+
+    @staticmethod
+    def from_yaml(yaml_path):
+        """
+        Factory method to create a Show object from yaml
+        """
+        with open(yaml_path, 'r') as yaml_file:
+            yaml_data = yaml.load(yaml_file, Loader=yaml.Loader)
+        return Show(**yaml_data)
+
     def _rfc5545_weekday(self):
         _recurrence_weekday = [ "MO", "TU", "WE", "TH", "FR", "SA", "SU" ]
         return _recurrence_weekday[self.start_date.weekday()]
@@ -133,7 +179,9 @@ class Show(GoogleMediaEvent):
             "summary": self.title,
             "start": { "date": self.start_date.isoformat() },
             "end": { "date": (self.start_date + datetime.timedelta(days=1)).isoformat() },
-            "recurrence": [f"RRULE:FREQ=WEEKLY;WKST=SU;COUNT={self.weeks};BYDAY={self._rfc5545_weekday()}"],
+            "recurrence": [
+                f"RRULE:FREQ=WEEKLY;WKST=SU;COUNT={self.weeks};BYDAY={self._rfc5545_weekday()}"
+            ],
         }
 
     def sort_val(self):
@@ -144,62 +192,87 @@ class Show(GoogleMediaEvent):
             return self.title      == other.title      \
                and self.start_date == other.start_date \
                and self.weeks      == other.weeks
-        else:
-            self_event = self.to_google_event()
-            return self_event["summary"]       == other["summary"]       \
-               and self_event["start"]["date"] == other["start"]["date"] \
-               and self_event["end"]["date"]   == other["end"]["date"]   \
-               and self_event["recurrence"]    == other["recurrence"]
+        self_event = self.to_google_event()
+        return self_event["summary"]       == other["summary"]       \
+           and self_event["start"]["date"] == other["start"]["date"] \
+           and self_event["end"]["date"]   == other["end"]["date"]   \
+           and self_event["recurrence"]    == other["recurrence"]
 
     def __str__(self):
         return f"{truncate(self.title, 26)} {self.start_date.strftime('%b %d, %Y')}"
 
+def get_objects_from_data(folder_name, factory):
+    """
+    Loads all of the objects defined in yaml files in ./data/{{folder_name}}
+    """
+    dir_path = os.path.join('data', folder_name)
+    return [factory(os.path.join(dir_path, filename)) for filename in os.listdir(dir_path)]
+
 
 def get_movies():
-    movies_dir = os.path.join('data', 'movies')
-    return [Movie(os.path.join(movies_dir, filename)) for filename in os.listdir(movies_dir)]
+    """
+    Gets all Movie objects defined in the yaml files in ./data/movies
+    """
+    return get_objects_from_data('movies', Movie.from_yaml)
 
 
 def get_shows():
-    shows_dir = os.path.join('data', 'shows')
-    return [Show(os.path.join(shows_dir, filename)) for filename in os.listdir(shows_dir)]
+    """
+    Gets all Show objects defined in the yaml files in ./data/shows
+    """
+    return get_objects_from_data('shows', Show.from_yaml)
 
 
 def truncate(string, length):
+    """
+    Truncates a string to a given length with "..." at the end if needed
+    """
     return string[:(length-3)].ljust(length, ".")
 
 
-def find(seq, f):
+def find(seq, predicate):
+    """
+    Finds the first element in seq that predicate return true for
+    """
     for item in seq:
-        if f(item):
+        if predicate(item):
             return item
     return None
 
 
-def create_google_event(progressTitle, items, existingEvents):
+def create_google_event(progress_title, items, existing_events):
+    """
+    Creates or Updates events if needed on the calendar based on the items objects
+    """
+    calendar_id = get_cal_id()
     progress = Progress(
-        TextColumn(progressTitle),
+        TextColumn(progress_title),
         BarColumn(),
         "[progress.percentage]{task.percentage:>3.0f}%",
         TimeElapsedColumn())
     items.sort(key=lambda i: i.sort_val())
     with progress:
         for item in progress.track(items):
-            event = find(existingEvents, lambda e: 'summary' in e and e['summary'] == item.title)
-            if event == None:
+            event = find(existing_events, lambda e: 'summary' in e and e['summary'] == item.title)
+            if event is None:
                 progress.print(f"[reset]{item}", "[red](Adding)")
-                events_service.insert(calendarId=calId, body=item.to_google_event()).execute()
+                EVENTS_SERVICE.insert(calendarId=calendar_id, body=item.to_google_event()).execute()
             elif item != event:
                 progress.print(f"[reset]{item}", "[yellow](Updating)")
-                events_service.update(calendarId=calId, eventId=event["id"], body=item.to_google_event()).execute()
+                EVENTS_SERVICE.update(
+                    calendarId=calendar_id,
+                    eventId=event["id"],
+                    body=item.to_google_event()).execute()
             else:
                 progress.print(f"[reset]{item}", "[cyan](Skipping)")
 
 
-calId = get_cal_id()
-events_service = build('calendar', 'v3', credentials=get_google_creds()).events()
+EVENTS_SERVICE = build('calendar', 'v3', credentials=get_google_creds()).events()
 
 def main():
+    """
+    Main method that updates the users google calendar
+    """
     events = get_google_events()
     create_google_event("[bold]Movies..", get_movies(), events)
     create_google_event("[bold]Shows...", get_shows(), events)
--- tests/main_test.py
+++ tests/main_test.py
@@ -16,15 +16,15 @@ def test_get_movies():
 
 @pytest.mark.parametrize("movie_path", os.listdir(os.path.join('data', 'movies')))
 def test_movies_yaml(movie_path):
-    movie = Movie(os.path.join('data', 'movies', movie_path))
+    movie = Movie.from_yaml(os.path.join('data', 'movies', movie_path))
     assert type(movie.title)        is str
     assert type(movie.release_date) is datetime.date
     assert type(movie.description)  is str
 
 
 def test_movies_equals():
-    movie1 = Movie({ "title": "MY TITLE", "release_date": datetime.date(2019, 4, 20), "description": "stuff happens"})
-    movie2 = Movie({ "title": "MY TITLE", "release_date": datetime.date(2019, 4, 20), "description": "stuff happens"})
+    movie1 = Movie(title= "MY TITLE", release_date= datetime.date(2019, 4, 20), description= "stuff happens")
+    movie2 = Movie(title= "MY TITLE", release_date= datetime.date(2019, 4, 20), description= "stuff happens")
     assert movie1 == movie1
     assert movie1 == movie2
     assert not movie1 != movie2
@@ -36,14 +36,14 @@ def test_movies_equals():
     { "title": "MY TITLE", "release_date": datetime.date(2019, 4, 20), "description": "nothing happens"},
 ])
 def test_movies_not_equals(movie_dict):
-    movie1 = Movie({ "title": "MY TITLE", "release_date": datetime.date(2019, 4, 20), "description": "stuff happens"})
-    movie2 = Movie(movie_dict)
+    movie1 = Movie(title= "MY TITLE", release_date= datetime.date(2019, 4, 20), description= "stuff happens")
+    movie2 = Movie(**movie_dict)
     assert movie1 != movie2
     assert not movie1 == movie2
 
 
 def test_movie_event_equals():
-    movie = Movie({ "title": "MY TITLE", "release_date": datetime.date(2019, 4, 20), "description": "stuff happens"})
+    movie = Movie(title= "MY TITLE", release_date= datetime.date(2019, 4, 20), description= "stuff happens")
     event = {
         "start": { "date": "2019-04-20" },
         "end": { "date": "2019-04-20" },
@@ -63,7 +63,7 @@ def test_movie_event_equals():
     { "start": { "date": "2019-04-20" }, "end": { "date": "2019-04-20" }, "summary": "MY TITLE", "description": "Something happens" },
 ])
 def test_movie_event_not_equals(event_dict):
-    movie = Movie({ "title": "MY TITLE", "release_date": datetime.date(2019, 4, 20), "description": "stuff happens"})
+    movie = Movie(title= "MY TITLE", release_date= datetime.date(2019, 4, 20), description= "stuff happens")
     assert movie != event_dict
     assert not movie == event_dict
 
@@ -75,15 +75,15 @@ def test_get_shows():
 
 @pytest.mark.parametrize("show_path", os.listdir(os.path.join('data', 'shows')))
 def test_shows_yaml(show_path):
-    show = Show(os.path.join('data', 'shows', show_path))
+    show = Show.from_yaml(os.path.join('data', 'shows', show_path))
     assert type(show.title)      is str
     assert type(show.start_date) is datetime.date
     assert type(show.weeks)      is int
 
 
 def test_shows_equals():
-    show1 = Show({ "title": "MY TITLE", "start_date": datetime.date(2019, 4, 20), "weeks": 7})
-    show2 = Show({ "title": "MY TITLE", "start_date": datetime.date(2019, 4, 20), "weeks": 7})
+    show1 = Show(title= "MY TITLE", start_date= datetime.date(2019, 4, 20), weeks= 7)
+    show2 = Show(title= "MY TITLE", start_date= datetime.date(2019, 4, 20), weeks= 7)
     assert show1 == show1
     assert show1 == show2
     assert not show1 != show1
@@ -96,14 +96,14 @@ def test_shows_equals():
     { "title": "MY TITLE", "start_date": datetime.date(2019, 4, 20), "weeks": 9},
 ])
 def test_shows_not_equals(show_dict):
-    show1 = Show({ "title": "MY TITLE", "start_date": datetime.date(2019, 4, 20), "weeks": 3})
-    show2 = Show(show_dict)
+    show1 = Show(title= "MY TITLE", start_date= datetime.date(2019, 4, 20), weeks= 3)
+    show2 = Show(**show_dict)
     assert not show1 == show2
     assert show1 != show2
 
 
 def test_show_event_equals():
-    show = Show({ "title": "MY TITLE", "start_date": datetime.date(2019, 4, 20), "weeks": 20})
+    show = Show(title= "MY TITLE", start_date= datetime.date(2019, 4, 20), weeks= 20)
     event = {
         "start": { "date": "2019-04-20" },
         "end": { "date": "2019-04-21" },
@@ -123,6 +123,6 @@ def test_show_event_equals():
     { "start": { "date": "2019-04-20" }, "end": { "date": "2019-04-21" }, "summary": "MY TITLE", "recurrence": f"RRULE:FREQ=WEEKLY;WKST=SU;COUNT=10;BYDAY=SA" },
 ])
 def test_show_event_not_equals(event_dict):
-    show = Show({ "title": "MY TITLE", "start_date": datetime.date(2019, 4, 20), "weeks": 6})
+    show = Show(title= "MY TITLE", start_date= datetime.date(2019, 4, 20), weeks= 6)
     assert not show == event_dict
     assert show != event_dict
