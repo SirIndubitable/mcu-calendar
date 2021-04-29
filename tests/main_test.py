31e214116fe938bdbdb024f5754db55ca04d7a1f
Matt Olson <SirIndubitable@gmail.com>
2021-04-28 23:48:42 -0500

    Remove test for google login, because the test stage doesn't have the google credentials

--- tests/main_test.py
+++ tests/main_test.py
@@ -4,10 +4,6 @@ import sys, os
 sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
 from mcu_calendar.main import *
 
-def test_google_login():
-    creds = get_google_creds()
-    assert creds
-
 
 def test_get_movies():
     movies = get_movies()
